16bafc1dd2ca0cb5781f4c4e223ebc1b54bfe3cf
jusikXL <125464973+jusikXL@users.noreply.github.com>
2023-02-20 19:30:00 +0100

    Organisation 0.1.0

---
+++ contracts/Organisation.sol
@@ -0,0 +1,43 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "./Register.sol";
+
+contract Organisation is Ownable {
+
+    address[] public registers;
+    string public metadata;
+
+    event RegisterDeployed (address register);
+    event OrganisationMetadataUpdated (address organisation, string metadata);
+
+
+    constructor (string memory _metadata) {
+        metadata = _metadata;
+    }
+
+    function deployRegister (
+        string memory _registerMetadata
+    )
+        public 
+        onlyOwner()
+    {
+        Register newRegister = new Register(address(this)/*address of organisation*/, _registerMetadata);
+        registers.push(address(newRegister));
+
+        emit RegisterDeployed(address(newRegister));
+    }
+
+    function updateMetadata (
+        string memory _metadata
+    ) 
+        public 
+        onlyOwner()
+    {
+        metadata = _metadata;
+        
+        emit OrganisationMetadataUpdated(address(this)/*address of organisation*/, _metadata);
+    }
+    
+}
