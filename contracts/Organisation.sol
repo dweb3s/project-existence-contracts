43295afedb8a829669632920ac6da413caea898a
jusikXL <125464973+jusikXL@users.noreply.github.com>
2023-02-20 20:23:38 +0100

    Organisation 0.1.1

--- contracts/Organisation.sol
+++ contracts/Organisation.sol
@@ -29,7 +29,7 @@ contract Organisation is Ownable {
         emit RegisterDeployed(address(newRegister));
     }
 
-    function updateMetadata (
+    function updateOrganisationMetadata (
         string memory _metadata
     ) 
         public 
