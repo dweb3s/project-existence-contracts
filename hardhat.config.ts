d24697c5be8d51d990c6be0b984a73f03ffcd17b
catAngent <97902025+catAngent@users.noreply.github.com>
2023-03-04 11:20:33 +0000

    add fantom_testnet network

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -12,6 +12,12 @@ const config: HardhatUserConfig = {
                 "d58a399d16035036f70830bb25dfe6312f578f8895708e11c53b563f526734ea", // fd0b8b95dfc4ab760e1018a04c2e98acb91d3058
                 "605d59030c374d54abca05631a1d231b7ad4cce6821a9e7270c236cbe61d23e3"  // e6dadd4b32abbbc8b47a8d0e4bbed17e45608ed0
             ]
+        },
+        fantom_testnet: {
+            url: `https://rpc.ankr.com/fantom_testnet`,
+            accounts: [
+                "ee60ae4a9ef7093f654fbee5f82a988d8aaed375972b199317ad2a6612bfc80f", // f6d98c56af286bd00aac313a5bf0c0f2f3c7d002
+            ]
         }
     }
 };
